14249822fad0210a4d0516ebe570a034b3337b89
marawangomaa <116531019+marawangomaa@users.noreply.github.com>
2025-07-23 10:58:54 +0300

    side components +

---
+++ CharityFE/src/app/Pages/sid-components/ContactPhone/contact-phone.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-phone',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-phone.component.html',
+  styleUrl: './contact-phone.component.scss'
+})
+export class ContactPhoneComponent {} 
---
+++ CharityFE/src/app/Pages/sid-components/NationalAddress/national-address.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-national-address',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './national-address.component.html',
+  styleUrl: './national-address.component.scss'
+})
+export class NationalAddressComponent {} 
